48b85355ddc63132fda41ec3e01ceff5334f943d
Gregory Igelmund <github.com@grekko.de>
2020-10-26 19:37:53 +0100

    mastery_persistence: Fix config test

--- apps/elixir.mastery_persistence/config/test.exs
+++ apps/elixir.mastery_persistence/config/test.exs
@@ -2,6 +2,6 @@ use Mix.Config
 
 config :mastery_persistence,
        MasteryPersistence.Repo,
-       database: "mastery_dev",
+       database: "mastery_test",
        hostname: "localhost",
        pool: Ecto.Adapters.SQL.Sandbox
