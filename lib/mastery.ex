2a3f52d466a7ddedfa4ce87315a04f4be9a62e89
Gregory Igelmund <github.com@grekko.de>
2020-10-26 17:07:14 +0100

    Integrate mastery_persistence

--- lib/mastery.ex
+++ lib/mastery.ex
@@ -3,6 +3,8 @@ defmodule Mastery do
   alias Mastery.Boundary.{TemplateValidator, QuizValidator}
   alias Mastery.Core.Quiz
 
+  @persistence_fn Application.get_env(:mastery, :persistence_fn)
+
   def build_quiz(fields) do
     with :ok <- QuizValidator.errors(fields),
          :ok <- GenServer.call(QuizManager, {:build_quiz, fields}),
@@ -26,12 +28,12 @@ defmodule Mastery do
     end
   end
 
-  def select_question(session) do
-    GenServer.call(session, :select_question)
+  def select_question(name) do
+    QuizSession.select_question(name)
   end
 
-  def answer_question(session, answer) do
-    GenServer.call(session, {:answer_question, answer})
+  def answer_question(name, answer, persistence_fn \\ @persistence_fn) do
+    QuizSession.answer_question(name, answer, persistence_fn)
   end
 
   def schedule_quiz(quiz, templates, start_at, end_at) do
