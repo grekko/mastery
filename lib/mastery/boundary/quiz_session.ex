5ca942a77349796cf762b49805f47f3c736572fe
Gregory Igelmund <github.com@grekko.de>
2020-10-26 19:38:30 +0100

    Mastery.Boundary.QuizSession: Provide child_spec/1 with `restart: :temporary`

--- lib/mastery/boundary/quiz_session.ex
+++ lib/mastery/boundary/quiz_session.ex
@@ -2,6 +2,14 @@ defmodule Mastery.Boundary.QuizSession do
   alias Mastery.Core.{Quiz, Response}
   use GenServer
 
+  def child_spec({quiz, email}) do
+    %{
+      id: {__MODULE__, {quiz.title, email}},
+      start: {__MODULE__, :start_link, [{quiz, email}]},
+      restart: :temporary
+    }
+  end
+
   def start_link({quiz, email}) do
     GenServer.start_link(__MODULE__, {quiz, email}, name: via({quiz.title, email}))
   end
